84b809fab62ea368dfc62670ca4a18691cceddd9
Sittijed Wongtichawat <sittijed.w@arise.tech>
2024-07-10 21:35:15 +0700

    Write test case and implement fizzBuzz logic for n = 2 should return 2

--- normalfizzbuzz/src/fizzBuzz.test.ts
+++ normalfizzbuzz/src/fizzBuzz.test.ts
@@ -8,4 +8,12 @@ describe('FizzBuzz', () => {
 
         expect(result).toEqual('1')
     })
+
+    it('n = 2 should return 2', () => {
+        const n: number = 2
+
+        const result: string = fizzBuzz(n)
+
+        expect(result).toEqual('2')
+    })
 })
